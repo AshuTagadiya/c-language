ef1ef5351fc5b24440bf0df3892c55d0a972b41d
AshuTagadiya <ashutagadiya02@gmail.com>
2023-05-01 17:18:04 +0530

    ch1 intro of c language

---
+++ ch1/A4-1-2.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+    clrscr();
+    printf("name\t\t:ashu tagadiya\n");
+    printf("cours\t\t:c language\n");
+    printf("age\t\t:17\n");
+    printf("contect no.\t:7862099320\n");
+    printf("hobby\t\t:\"music\"");
+    getch();
+}
