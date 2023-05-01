af33d542548448b1e7ea00d42de9ce1b07aa8195
AshuTagadiya <ashutagadiya02@gmail.com>
2023-05-01 17:08:27 +0530

    Ch3

---
+++ ch3/A3-1-1.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+    clrscr();
+    printf("-----\n");
+    printf("a   |\n");
+    printf("s   |\n");
+    printf("h   |\n");
+    printf("u   |\n");
+    printf("-----\n");
+    getch();
+}
